d17cef3e2c10123d93fee34b53442bdb164885b6
jadon hoosier <hoosijad000@warren.k12.in.us>
2024-08-28 10:16:51 -0400

    imput6

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ name = input("What is your name: ")
 #INSTRUCTION:  write an input
 # statement that asks for
 # number of wookies and store as wookiees
+droids = input ("number of droids and store as droids")
+wookiees = input ("number of wookiees and store as wookiees")
 print(name, "wants to meet", droids, "droids and ", wookiees, "Wookiees!")
-name = input ("number of droids and store as droids")
-name = input ("number of wookiees and store as wookiees")
